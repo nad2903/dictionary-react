77d671e302c4ef007c60bcfc387dc55a1533dd3b
nad2903 <77510852+nad2903@users.noreply.github.com>
2021-04-19 17:58:16 -0400

    Added search engine function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState }from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+
+  let [searchedWord, setSearchedWord] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(searchedWord);
+  }
+
+  function updateSearch(event) {
+    setSearchedWord(event.target.value);
+  }
     return (
       <div className="Dictionary">
         <div className="container">
           <h1>Dictionary</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="row">
               <div className="col-9">
               <input 
@@ -15,6 +26,7 @@ export default function Dictionary() {
                 placeholder="Search a word..."
                 autoComplete={false}
                 autoFocus={true}
+                onChange = {updateSearch}
               />
               </div>
               <div className="col-3">
